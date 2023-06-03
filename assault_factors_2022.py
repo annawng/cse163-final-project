f97b9f84bd5e32dc160c9cc2cec598ca3b5fe7a3
Error-By-Night <36522345+Error-By-Night@users.noreply.github.com>
2023-06-03 13:56:29 -0700

    Update assault_factors_2022.py

--- assault_factors_2022.py
+++ assault_factors_2022.py
@@ -13,8 +13,9 @@ def plot_assault_factors_2022(data: pd.DataFrame) -> None:
     # df['date_col'].dt.strftime('%H:%M') another possible way to filter time
     data['Time'] = data['Offense Start DateTime'].dt.time.astype(int)
     # data = data.groupby['Offense Parent Group']('MCPP')
-    time_data = data.groupby['Time']('Hour').count()
-    fig = px.pie(time_data, values='time_count', names='Hour', title='Number of Crimes for each Hour')
+    data = data['Offense Parent Group'] == "Assault Offenses"
+    data['time_count'] = data.groupby['Time']('Hour').count()
+    fig = px.pie(data, values='time_count', names='Hour', title='Number of Crimes for each Hour')
     fig.show()
 
 
@@ -22,13 +23,18 @@ def merge_data(shp_file: str, csv_file: str) -> gpd.DataFrame:
     geospatial_data = gpd.read_file(shp_file)
     spd_data = pd.read_csv(csv_file)
 
-    # figure out how to merge
-    merged_gdf = geospatial_data.merge(spd_data, left_on='sector', right_on='sector', how='left')
+    # if doesnt work try how=inner or on
+    merged_gdf = geospatial_data.merge(spd_data, on='sector' how='outer')
 
     return merged_gdf
 
 
 def plot_geo_data(sector_data: gpd.DataFrame) -> None:
     fig, ax = plt.subplots(1)
-    sector_filter =
+    sector_filter = sector_data[['sector', 'geometry', 'Offense Parent Group']]
+    sector_filter = sector_filter['Offense Parent Group'] == "Assault Offenses"
+    sector_filter = sector_filter.groupby['Offense Parent Group']('Offense Parent Group').count()
+    sector_filter.plot(ax=ax, column='Offense Parent Group', legend=True)
+    plt.title("Assault Offenses by Sector")
+    fig.savefig('assault_offense_map.png')
 
