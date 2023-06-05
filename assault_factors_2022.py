9a3b6d3143f35343abe2892aa76a0060744fb4d5
Anna <wanganna@uw.edu>
2023-06-04 18:14:06 -0700

    Work on assault factors 2022

--- assault_factors_2022.py
+++ assault_factors_2022.py
@@ -1,40 +1,57 @@
 import pandas as pd
 import geopandas as gpd
-# import numpy as np
+import matplotlib.pyplot as plt
 import plotly.express as px
 
 
-# What factors resulted in the greatest amount of assault offenses in Seattle in 2022?
-# Time -> Pie Chart, MCPP -> Bar Chart, Sector -> GeoPandas
+# What factors resulted in the greatest amount of assault offenses in
+# Seattle in 2022?
 def plot_assault_factors_2022(data: pd.DataFrame) -> None:
-    # Filter data by time of offense, only assault cases, and MCPP (area)
-    data = data[['Offense Start DateTime', 'Offense Parent Group','MCPP']].dropna()
-    data['Offense Start DateTime'] = pd.to_datetime(data['Offense Start DateTime'])
-    # df['date_col'].dt.strftime('%H:%M') another possible way to filter time
-    data['Time'] = data['Offense Start DateTime'].dt.time.astype(int)
-    # data = data.groupby['Offense Parent Group']('MCPP')
-    data = data['Offense Parent Group'] == "Assault Offenses"
-    data['time_count'] = data.groupby['Time']('Hour').count()
-    fig = px.pie(data, values='time_count', names='Hour', title='Number of Crimes for each Hour')
-    fig.show()
+    data = data[['Offense Start DateTime',
+                 'Offense Parent Group']].dropna()
+    data['Offense Start DateTime'] = \
+        pd.to_datetime(data['Offense Start DateTime'])
+    data['Hour'] = data['Offense Start DateTime'].dt.hour
+    data = data[data['Offense Parent Group'] == "ASSAULT OFFENSES"]
+    data = (
+        data.groupby('Hour')['Hour'].count()
+        .reset_index(name="Count")
+    )
+    fig = px.bar(data, x='Hour', y='Count',
+                 title='Number of Assault Offenses Per Hour',
+                 labels={
+                    'Hour': 'Time',
+                 })
+    fig.update_layout(
+        xaxis=dict(
+            tickmode='array',
+            tickvals=list(range(25)),
+            ticktext=[f'{12 if i == 0 else i}am'
+                      if i < 12
+                      else f'{12 if i == 12 else i % 12}pm' for i in range(25)]
+        )
+    )
+
+    fig.write_image('./results/assault_factors_2022.jpg')
 
 
-def merge_data(shp_file: str, csv_file: str) -> gpd.DataFrame:
+def merge_data(shp_file: str, csv_file: str) -> gpd.GeoDataFrame:
     geospatial_data = gpd.read_file(shp_file)
     spd_data = pd.read_csv(csv_file)
 
     # if doesnt work try how=inner or on
-    merged_gdf = geospatial_data.merge(spd_data, on='sector' how='outer')
-
+    merged_gdf = geospatial_data.merge(spd_data, left_on='sector',
+                                       right_on='Sector', how='outer')
     return merged_gdf
 
 
-def plot_geo_data(sector_data: gpd.DataFrame) -> None:
+def plot_geo_data(data: gpd.GeoDataFrame) -> None:
+    data.plot()
     fig, ax = plt.subplots(1)
-    sector_filter = sector_data[['sector', 'geometry', 'Offense Parent Group']]
-    sector_filter = sector_filter['Offense Parent Group'] == "Assault Offenses"
-    sector_filter = sector_filter.groupby['Offense Parent Group']('Offense Parent Group').count()
-    sector_filter.plot(ax=ax, column='Offense Parent Group', legend=True)
-    plt.title("Assault Offenses by Sector")
-    fig.savefig('assault_offense_map.png')
-
+    # data = data[['sector', 'geometry', 'Offense Parent Group']]
+    # data = data[data['Offense Parent Group'] == "ASSAULT OFFENSES"]
+    # data = data.dissolve(by='sector', aggfunc='count')
+    # print(data.head(10))
+    # data.plot(ax=ax, column='Count', legend=True)
+    # plt.title("Assault Offenses by Sector")
+    plt.savefig('./results/assault_offense_map.png')
--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import pandas as pd
 
 from assault_offenses_over_time import plot_assault_offenses_over_time
-# from assault_factors_2022 import plot_assault_factors_2022
+from assault_factors_2022 import plot_assault_factors_2022
+from assault_factors_2022 import merge_data
+from assault_factors_2022 import plot_geo_data
+
 
 DATASET = './data/SPD_Crime_Data__2008-Present.csv'
+GEO_DATASET = './data/Seattle_Police_Beats_2018-Present.shp'
 
 
 def main():
-    data = pd.read_csv(DATASET)
-    plot_assault_offenses_over_time(data)
+    # data = pd.read_csv(DATASET)
+    # plot_assault_offenses_over_time(data)
+    # plot_assault_factors_2022(data)
+    geodata = merge_data(GEO_DATASET, DATASET)
+    plot_geo_data(geodata)
 
 
 if __name__ == '__main__':
