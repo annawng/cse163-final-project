56dfed977095f421c9e1c61581d32bc7c0679e02
Joey <36522345+Error-By-Night@users.noreply.github.com>
2023-06-05 17:30:16 -0700

    add new test dataset tests

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from predicting_offenses import predict_offense_type
 
 
 DATASET = './data/SPD_Crime_Data__2008-Present.csv'
+TESTDATA = './data/SPD_Test_Data.csv'
 
 
 def main():
@@ -25,6 +26,12 @@ def main():
     plot_assaults_by_time_2022(data)
     plot_assaults_by_location_2022(data)
     predict_offense_type(data)
+    
+    test_data = pd.read_csv(TESTDATA)
+    plot_assault_offenses_over_time(test_data)
+    plot_assaults_by_time_2022(test_data)
+    plot_assaults_by_location_2022(test_data)
+    predict_offense_type(test_data)
 
 
 if __name__ == '__main__':
